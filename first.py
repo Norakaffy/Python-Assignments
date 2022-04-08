66dec2ffb849fffa1f5e4541ecf181f32b5e2a43
Nora Mbee <Kafonya.mbee@gmail.com>
2022-04-08 14:59:16 +0100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,45 @@
+
+
+import math
+
+a = 10.2
+
+print(a ** 3)
+print (a%3)
+
+sqrt = math.sqrt(25)
+print(sqrt)
+p = math.pow(2, 4)
+
+print( math.ceil(2.378) )
+print( math.floor(2.99) )
+print( round (2.99) )
+print(p)
+
+firstname="John"
+lastname="james"
+
+fullname = firstname + lastname
+print(fullname)
+
+
+firstname = "John"
+lastname="james"
+num=1
+fullname = firstname + " " + lastname + str(num)
+print(fullname)
+
+name = "Andrew"
+print ( name[0:3])
+print ( name[0:4])
+print( name [3]+name[5])
+
+name = input("Enter Your Name")
+print("Hello, "+name)
+nickname = input("Enter Your Nickname")
+print("Hello, "+nickname)
+gender = input ("Enter Your Gender")
+print("Hello, "+gender)
+
+
+
